29827f58c137cb4e0cee13eb68c5f53696ebab3f
Sanket Navnath Tupe <82076364+sanket-uptycs@users.noreply.github.com>
2024-02-13 20:59:48 +0530

    Update mapper.go

--- drivers/golang/age/mapper.go
+++ drivers/golang/age/mapper.go
@@ -23,7 +23,7 @@ import (
 	"reflect"
 	"strings"
 	"github.com/antlr/antlr4/runtime/Go/antlr/v4"
-	"github.com/apache/age/drivers/golang/parser"
+	"github.com/sanket-uptycs/age/drivers/golang/parser"
 )
 
 type AGMapper struct {
