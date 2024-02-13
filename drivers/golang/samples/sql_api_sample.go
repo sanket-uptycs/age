1790121db94d20f765604bc24ecd5997f970d886
Sanket Navnath Tupe <82076364+sanket-uptycs@users.noreply.github.com>
2024-02-13 21:00:36 +0530

    Update sql_api_sample.go

--- drivers/golang/samples/sql_api_sample.go
+++ drivers/golang/samples/sql_api_sample.go
@@ -22,7 +22,7 @@ import (
 	"database/sql"
 	"fmt"
 
-	"github.com/apache/age/drivers/golang/age"
+	"github.com/sanket-uptycs/age/drivers/golang/age"
 )
 
 // Do cypher query to AGE with database/sql Tx API transaction control
